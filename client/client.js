1a77ef454a970885a9f161344935c20d882cfedd
MattSchaefer24 <hkvd57@durham.ac.uk>
2022-02-24 21:47:25 +0000

    Further improvements

--- client/client.js
+++ client/client.js
@@ -1,11 +1,30 @@
+//This is setting button to our submit button on our HTML page
 var button = document.getElementById('submit')
 
+
+//Stores all the neccesary information when the submit button is clicked 
+
 button.addEventListener('click', async function(event){
+
+    //Next couple of line will keep track of which radio button is selected
+    var media = ""
+    if(document.getElementById('song').checked) {
+        media = "song"    }
+    else if(document.getElementById('album').checked){
+        media = "album"
+    }
+    else{
+        media = "lyric"
+    }
+
+    //This is going to combine all the input we need and send a request to the server
     var endpoint = document.getElementById('input').value
-    let endpoint_url = 'http://127.0.0.1:8090/' + endpoint
+    var endpoint_url = 'http://127.0.0.1:8090/' + endpoint +'/' + media
+
     try{
         let response = await fetch(endpoint_url)
         let body = await response.text()
+        alert(body)
         document.getElementById('content').innerHTML = body
     }catch(e){
         alert(e)
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express')
 
 var app = express()
 
+data = JSON.parse.(taylor_swift_lyrics.json)
 app.use(express.static('client'))
 
 
@@ -13,4 +14,10 @@ app.get('/test', function(req, resp){
     resp.send('Test function worked')
 })
 
+app.get('/:user/:media', function(req, resp){
+    let user = req.params['user']
+    let media = req.params['media']
+    let album = data['media']
+    resp.send(album)
+})
 app.listen(8090)
